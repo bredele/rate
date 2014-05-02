33a48752f6cf70d136b857bd7c267225401c5862
bredele <olivier.wietrich@gmail.com>
2014-05-01 21:38:39 -0600

    rate works as a codec

---
+++ index.js
@@ -0,0 +1,25 @@
+
+/**
+ * Change data channel speed limitation.
+ *
+ * 
+ * Examples:
+ *
+ *   peer.codec(rate());
+ *   peer.codec(rate(100));
+ *
+ * @param {Number} rate
+ * @return {Function} codec
+ * @api public
+ */
+
+module.exports = function(rate) {
+  rate = rate || 1638400;
+  return function(sdp) {
+    var split = sdp.split("b=AS:30");
+    if(split.length > 1) {
+      sdp = split[0] + "b=AS:" + rate + split[1];
+    }
+    return sdp;
+  };
+};
